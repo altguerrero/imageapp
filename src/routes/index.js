a0a56dc445dc8c1feb56ab6b4f9c991ddd8312ca
Jose Guerrero <josealberto.guerreroayala@gmail.com>
2019-08-21 16:20:30 -0500

    Routes finalizados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const homeCtrl = require("../controllers/home.controller");
+const imageCtrl = require("../controllers/image.controller");
+
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send("yolo mero");
-  });
+
+  router.get('/', homeCtrl.index);
+  router.get('/images/:image_id', imageCtrl.index);
+  router.post('/images', imageCtrl.create);
+  router.post('/images/:image_id/like', imageCtrl.like);
+  router.post('/images/:image_id/comment', imageCtrl.comment);
+  router.delete('/images/:image_id', imageCtrl.remove);
+
+  app.use(router);
+
 };
