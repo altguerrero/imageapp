fb2cede15b3c500f525b5516f2d52c96c6634285
Jose Guerrero <josealberto.guerreroayala@gmail.com>
2019-08-21 16:23:15 -0500

    Configuracion del servidor finalizada

--- src/server/config.js
+++ src/server/config.js
@@ -1,51 +1,43 @@
-const path = require("path");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const errorHandler = require('errorhandler');
+const exphbs = require('express-handlebars');
+const multer = require('multer');
 
-const morgan = require("morgan");
-const multer = require("multer");
-const express = require("express");
-
-const errorHandler = require("errorHandler");
-
-const routes = require("../routes/index");
+const routes = require('../routes');
 
 module.exports = app => {
-  // settings
-  app.set("port", process.env.PORT || 3000);
-  app.set("views", path.join(__dirname, "views"));
-  app.engine(
-    // confing of handlebars
-    ".hbs",
-    exphbs({
-      defaultLayout: "main",
-      partialsDir: path.join(app.get("views"), "partials"),
-      layoutsDir: path.join(app.get("views"), "layouts"),
-      extname: ".hbs",
-      helpers: require("./helpers")
-    })
-  );
-  app.set('view engine', '.hbs')
 
-  // middlewaresr
-  app.use(morgan("dev"));
-  app.use(
-    multer({ dest: path.join(__dirname, "../public/upload/temp") }).single(
-      "image"
-    )
-  );
-  app.use(express.urlencode({ extended: false }));
+  // Settings
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', path.join(__dirname, '../views'));
+  app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./helpers'),
+    extname: '.hbs'
+  }));
+  app.set('view engine', '.hbs');
+  app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'));
+
+  // middlewares
+  app.use(morgan('dev'));
+  app.use(express.urlencoded({extended: false}));
   app.use(express.json());
 
-  // routes
+  // Routes
   routes(app);
 
-  // static files
-  app.use("/public", express.static(path.join(__dirname, "../public")));
+  // Static files
+  app.use('/public', express.static(path.join(__dirname, '../public')));
 
-  // errorhandler
-  if (app.get("env") === "development") {
-    app.use(errorHandler);
+  // Error Handling
+  if('development' === app.get('env')) {
+    app.use(errorHandler());
   }
 
   return app;
+
 };
